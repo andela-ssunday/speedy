56fa35a13a9964cc54701705de80d9da39f5569a
Stephen <stephen.sunday@andela.co>
2015-10-06 12:00:29 +0100

    Fix fk issue on production

--- db/migrate/20151002094638_add_index_to_articles.rb
+++
@@ -1,5 +0,0 @@
-# class AddIndexToArticles < ActiveRecord::Migration
-#   def change
-#     add_index :articles, :author_id
-#   end
-# end
--- db/migrate/20151002113010_add_indices_to_authors.rb
+++
@@ -1,5 +0,0 @@
-# class AddIndicesToAuthors < ActiveRecord::Migration
-#   def change
-#     add_index :authors, [:id, :name]
-#   end
-# end
