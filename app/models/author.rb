a87144230a8a65c8ff29ad8044e17cbb03b87324
Stephen <stephen.sunday@andela.co>
2015-10-07 13:16:23 +0100

    Fix postgress issue in production

--- app/models/author.rb
+++ app/models/author.rb
@@ -18,11 +18,11 @@ class Author < ActiveRecord::Base
   end
 
   def self.most_prolific_writer
-    self.find_by_sql("select auth.name, count(art.id) as size from articles as art, authors as auth where art.author_id = auth.id order by size desc limit 1").first
+    self.find_by_sql("select count(art.id) as size , auth.name from articles as art, authors as auth where art.author_id = auth.id group by auth.name order by size desc limit 1").first
   end
 
   def self.with_most_upvoted_article
-    self.find_by_sql("select auth.name, art.upvotes as size from articles as art, authors as auth where art.author_id = auth.id order by size desc limit 1").first.name
+    self.find_by_sql("select auth.name, art.upvotes as size from articles as art, authors as auth where art.author_id = auth.id group by auth.name order by size desc limit 1").first.name
   end
 
 
